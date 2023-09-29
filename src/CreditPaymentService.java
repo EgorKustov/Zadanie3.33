d152b30c9987a7d5648e1c3d7a67af6c8a47738e
Testik <egorkustov@icloud.com>
2023-09-29 20:08:00 +0300

    Second commit

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,10 +1,9 @@
 public class CreditPaymentService {
 
     public double calculate(double loanTermInMonths, double amountOfCredit, double loanInterestRate) {
-        double monthlyPayment;
-        double pow = Math.pow(1 + (loanInterestRate / (12 * 100)), loanTermInMonths);
-        monthlyPayment = (amountOfCredit * ((loanInterestRate / (12 * 100)) * pow)) / (pow - 1);
-        return monthlyPayment;
+        double loanInterestRatePerMonth = (loanInterestRate / (12 * 100));
+        double pow = Math.pow(1 + (loanInterestRatePerMonth), loanTermInMonths);
+        return (amountOfCredit * (loanInterestRatePerMonth * pow)) / (pow - 1);
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
-        double loanTermInMonths = 12; //Срок кредита в месяцах
+        double loanTermInMonths = 24; //Срок кредита в месяцах
         double amountOfCredit = 1_000_000; //Сумма кредита в рублях
         double loanInterestRate = 9.99; //Процентная ставка
 
